c7ba396e7a3938e0c82e3e44b52ad41411ca51f0
radhikagupta25 <radhikaaaguptaaa25@gmail.com>
2026-06-25 22:13:14 +0530

    Initialization of react-router-dom

--- Radhika Gupta-Portfolio/src/App.jsx
+++ Radhika Gupta-Portfolio/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className='bg-amber-400'>App</div>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="skills" element={<Skills />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
